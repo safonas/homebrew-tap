9418be621455e4044780330cce55553a15e6f316
Eugenij Safonov <ja.safonau@protonmail.com>
2026-09-11 12:49:49 +0200

    ocid: bump to v0.3.0

--- Formula/ocid.rb
+++ Formula/ocid.rb
@@ -1,8 +1,8 @@
 class Ocid < Formula
   desc "Local-first, peer-to-peer distribution of OCI container images powered by iroh"
   homepage "https://github.com/safonas/ocid"
-  url "https://github.com/safonas/ocid/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "4d75b55d37ed006b5d5495a1f8120fcc6908784f63ed5c0f29bbca9157d68ea3"
+  url "https://github.com/safonas/ocid/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "753909c3cf3ac107c9102b0186786214d2939b689cb41f48be3581dd26e3a9c7"
   license "GPL-3.0-or-later"
   head "https://github.com/safonas/ocid.git", branch: "main"
 
@@ -11,6 +11,7 @@ class Ocid < Formula
   def install
     system "cargo", "install", *std_cargo_args(path: "crates/ocid")
     system "cargo", "install", *std_cargo_args(path: "crates/ocictl")
+    system "cargo", "install", *std_cargo_args(path: "crates/ocitop")
   end
 
   test do
