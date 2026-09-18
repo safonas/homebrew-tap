78eece0d2669bf30a2d1838994123ee2dcdc7012
Eugenij Safonov <ja.safonau@protonmail.com>
2026-09-18 12:50:00 +0200

    ocid: bump to v0.5.1

--- Formula/ocid.rb
+++ Formula/ocid.rb
@@ -1,8 +1,8 @@
 class Ocid < Formula
   desc "Local-first, peer-to-peer distribution of OCI container images powered by iroh"
   homepage "https://github.com/safonas/ocid"
-  url "https://github.com/safonas/ocid/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "5db6febbfa5442aab6d14caa87aafe597b208acbd1ffd521fe29010aebd986d6"
+  url "https://github.com/safonas/ocid/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "4dd383c81689c72634fc66f8efe423d2914ba4f4c816e1f8e0add7c303d5d6fe"
   license "GPL-3.0-or-later"
   head "https://github.com/safonas/ocid.git", branch: "main"
 
